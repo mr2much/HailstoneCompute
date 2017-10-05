643af8f3b8327d3e4e83f6a922e91c68463bd715
Mr2Much <anubis.lockward@gmail.com>
2017-10-04 21:30:56 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+/*
+
+Hailstone Sequence
+
+As a running example, we're going to explore the hailstone sequence, which is defined as follows.
+Starting with a number n, the next number in the sequence is n/2 if n is even, or 3n+1 if n is odd.
+The sequence ends when it reaches 1. Here are some examples:
+
+        2, 1
+        3, 10, 5, 16, 8, 4, 2, 1
+        4, 2, 1
+        2n, 2n-1 , ... , 4, 2, 1
+        5, 16, 8, 4, 2, 1
+        7, 22, 11, 34, 17, 52, 26, 13, 40, ...? (where does this stop?)
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Well hello there");
+    }
+}
